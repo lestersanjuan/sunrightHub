fefcb5368cafc0ffede6db2ab13f74a80df31f1b
lestersanjuan <lsanjua1@uci.edu>
2025-02-03 19:44:54 -0800

    learning

--- SunrightHub/src/App.tsx
+++ SunrightHub/src/App.tsx
@@ -1,7 +1,32 @@
-import Message from'./Message';
+import { useState } from "react";
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["NY", "SF", "TOKYO", "London", "meow"];
 
-function App(){
-  return <div><Message/></div>;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <div>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
