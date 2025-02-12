d514f843491f030d456185055c1cf9bcd1bdb3bd
lestersanjuan <lsanjua1@uci.edu>
2025-02-11 21:24:38 -0800

    tried adding table

--- SunrightHub/src/components/Inventory/Inventory.tsx
+++ SunrightHub/src/components/Inventory/Inventory.tsx
@@ -1,6 +1,28 @@
 
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+//To create a table entry/cell, make it easier, something to also put on top
+function createDataEntry(
+    BeginningOfWeek: number, // Beginning of Week, auto-update logic needs to be implemented separately
+    InventoryAdded: number, 
+    EndOfWeek: number,
+    OrderByBox: number // Order in units, needs additional logic if required
+) {
+    let Usage = BeginningOfWeek + InventoryAdded - EndOfWeek;
+    return { BeginningOfWeek, InventoryAdded, EndOfWeek, Usage, OrderByBox };
+}
+
 
 function Inventory() {
+
+
     return(
         <h1>Inventory!!</h1>
     ) 
