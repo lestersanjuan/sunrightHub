1a67c4d0ba607d6591e1766d7365a981380ee45f
lestersanjuan <lsanjua1@uci.edu>
2025-02-17 21:49:24 -0800

    reinstalled packages got some stuff working, added more stuff into inventory

--- SunrightHub/src/components/DailyReport/DailyReport.tsx
+++ SunrightHub/src/components/DailyReport/DailyReport.tsx
@@ -79,175 +79,346 @@ function DailyReport() {
   };
 
   return (
-    <Paper className="daily-report">
-      <Typography variant="h4" align="center" gutterBottom>
-        Daily Report
-      </Typography>
-
-      {/* Store Operations Section */}
-      <div className="section">
-        <Typography variant="h6" gutterBottom className="section-heading">
-          Store Operations
+    <>
+      <Paper className="daily-report">
+        <Typography variant="h4" align="center" gutterBottom>
+          Daily Report
         </Typography>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={12} sm={6}>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker
-                label="Select Date"
-                views={["year", "month", "day"]}
-                value={selectedDate}
-                onChange={handleDateChange}
-                renderInput={(params) => <TextField {...params} fullWidth />}
+
+        {/* Store Operations Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Store Operations
+          </Typography>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Select Date"
+                  views={["year", "month", "day"]}
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} fullWidth />}
+                />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.teaQuality}
+                    onChange={handleCheckboxChange}
+                    name="teaQuality"
+                  />
+                }
+                label="Tea Quality Check"
               />
-            </LocalizationProvider>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.bobaQuality}
+                    onChange={handleCheckboxChange}
+                    name="bobaQuality"
+                  />
+                }
+                label="Boba Quality Check"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.weeklyLeadership}
+                    onChange={handleCheckboxChange}
+                    name="weeklyLeadership"
+                  />
+                }
+                label="Weekly Leadership notes communicated with team"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={formValues.teaQuality}
-                  onChange={handleCheckboxChange}
-                  name="teaQuality"
+        </div>
+
+        {/* General Notes Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            General Notes
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="shiftLeadsSoups"
+                label="Shift Leads/Soups"
+                multiline
+                fullWidth
+                value={formValues.shiftLeadsSoups}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="generalNotes"
+                label="General Notes"
+                multiline
+                rows={4}
+                fullWidth
+                value={formValues.generalNotes}
+                onChange={handleTextChange}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        {/* Employee Performance Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Employee Performance
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="anyoneLate"
+                label="Anyone Late? ___Minutes"
+                multiline
+                fullWidth
+                value={formValues.anyoneLate}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="employeePerformance"
+                label="Employee Performance"
+                multiline
+                fullWidth
+                rows={4}
+                value={formValues.employeePerformance}
+                onChange={handleTextChange}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        {/* Previous Shift Lead/Soup Comments Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Previous Shift Lead/Soup Comments
+          </Typography>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TextField
+                name="previousSupervisor"
+                label="Refills Done? Previous Supervisor?"
+                multiline
+                fullWidth
+                value={formValues.previousSupervisor}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="previousShiftNotes"
+                label="Previous Shift Notes"
+                multiline
+                fullWidth
+                rows={4}
+                value={formValues.previousShiftNotes}
+                onChange={handleTextChange}
+              />
+            </Grid>
+          </Grid>
+        </div>
+
+        {/* Customer Comments Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Customer Comments
+          </Typography>
+          <TextField
+            name="customerComments"
+            label="Comments and if was solved?"
+            multiline
+            fullWidth
+            value={formValues.customerComments}
+            onChange={handleTextChange}
+          />
+        </div>
+
+        {/* Save Button */}
+        <div className="save-button-container">
+          <Button variant="contained" onClick={saveValue}>
+            Save
+          </Button>
+        </div>
+      </Paper>
+      <Paper className="daily-report">
+        <Typography variant="h4" align="center" gutterBottom>
+          Daily Report
+        </Typography>
+
+        {/* Store Operations Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Store Operations
+          </Typography>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Select Date"
+                  views={["year", "month", "day"]}
+                  value={selectedDate}
+                  onChange={handleDateChange}
+                  renderInput={(params) => <TextField {...params} fullWidth />}
                 />
-              }
-              label="Tea Quality Check"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={formValues.bobaQuality}
-                  onChange={handleCheckboxChange}
-                  name="bobaQuality"
-                />
-              }
-              label="Boba Quality Check"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={formValues.weeklyLeadership}
-                  onChange={handleCheckboxChange}
-                  name="weeklyLeadership"
-                />
-              }
-              label="Weekly Leadership notes communicated with team"
-            />
+              </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.teaQuality}
+                    onChange={handleCheckboxChange}
+                    name="teaQuality"
+                  />
+                }
+                label="Tea Quality Check"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.bobaQuality}
+                    onChange={handleCheckboxChange}
+                    name="bobaQuality"
+                  />
+                }
+                label="Boba Quality Check"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={formValues.weeklyLeadership}
+                    onChange={handleCheckboxChange}
+                    name="weeklyLeadership"
+                  />
+                }
+                label="Weekly Leadership notes communicated with team"
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </div>
+        </div>
 
-      {/* General Notes Section */}
-      <div className="section">
-        <Typography variant="h6" gutterBottom className="section-heading">
-          General Notes
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              name="shiftLeadsSoups"
-              label="Shift Leads/Soups"
-              multiline
-              fullWidth
-              value={formValues.shiftLeadsSoups}
-              onChange={handleTextChange}
-            />
+        {/* General Notes Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            General Notes
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="shiftLeadsSoups"
+                label="Shift Leads/Soups"
+                multiline
+                fullWidth
+                value={formValues.shiftLeadsSoups}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="generalNotes"
+                label="General Notes"
+                multiline
+                rows={4}
+                fullWidth
+                value={formValues.generalNotes}
+                onChange={handleTextChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="generalNotes"
-              label="General Notes"
-              multiline
-              rows={4}
-              fullWidth
-              value={formValues.generalNotes}
-              onChange={handleTextChange}
-            />
-          </Grid>
-        </Grid>
-      </div>
+        </div>
 
-      {/* Employee Performance Section */}
-      <div className="section">
-        <Typography variant="h6" gutterBottom className="section-heading">
-          Employee Performance
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              name="anyoneLate"
-              label="Anyone Late? ___Minutes"
-              multiline
-              fullWidth
-              value={formValues.anyoneLate}
-              onChange={handleTextChange}
-            />
+        {/* Employee Performance Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Employee Performance
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                name="anyoneLate"
+                label="Anyone Late? ___Minutes"
+                multiline
+                fullWidth
+                value={formValues.anyoneLate}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="employeePerformance"
+                label="Employee Performance"
+                multiline
+                fullWidth
+                rows={4}
+                value={formValues.employeePerformance}
+                onChange={handleTextChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="employeePerformance"
-              label="Employee Performance"
-              multiline
-              fullWidth
-              rows={4}
-              value={formValues.employeePerformance}
-              onChange={handleTextChange}
-            />
-          </Grid>
-        </Grid>
-      </div>
+        </div>
 
-      {/* Previous Shift Lead/Soup Comments Section */}
-      <div className="section">
-        <Typography variant="h6" gutterBottom className="section-heading">
-          Previous Shift Lead/Soup Comments
-        </Typography>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <TextField
-              name="previousSupervisor"
-              label="Refills Done? Previous Supervisor?"
-              multiline
-              fullWidth
-              value={formValues.previousSupervisor}
-              onChange={handleTextChange}
-            />
+        {/* Previous Shift Lead/Soup Comments Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Previous Shift Lead/Soup Comments
+          </Typography>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <TextField
+                name="previousSupervisor"
+                label="Refills Done? Previous Supervisor?"
+                multiline
+                fullWidth
+                value={formValues.previousSupervisor}
+                onChange={handleTextChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="previousShiftNotes"
+                label="Previous Shift Notes"
+                multiline
+                fullWidth
+                rows={4}
+                value={formValues.previousShiftNotes}
+                onChange={handleTextChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="previousShiftNotes"
-              label="Previous Shift Notes"
-              multiline
-              fullWidth
-              rows={4}
-              value={formValues.previousShiftNotes}
-              onChange={handleTextChange}
-            />
-          </Grid>
-        </Grid>
-      </div>
+        </div>
 
-      {/* Customer Comments Section */}
-      <div className="section">
-        <Typography variant="h6" gutterBottom className="section-heading">
-          Customer Comments
-        </Typography>
-        <TextField
-          name="customerComments"
-          label="Comments and if was solved?"
-          multiline
-          fullWidth
-          value={formValues.customerComments}
-          onChange={handleTextChange}
-        />
-      </div>
+        {/* Customer Comments Section */}
+        <div className="section">
+          <Typography variant="h6" gutterBottom className="section-heading">
+            Customer Comments
+          </Typography>
+          <TextField
+            name="customerComments"
+            label="Comments and if was solved?"
+            multiline
+            fullWidth
+            value={formValues.customerComments}
+            onChange={handleTextChange}
+          />
+        </div>
 
-      {/* Save Button */}
-      <div className="save-button-container">
-        <Button variant="contained" onClick={saveValue}>
-          Save
-        </Button>
-      </div>
-    </Paper>
+        {/* Save Button */}
+        <div className="save-button-container">
+          <Button variant="contained" onClick={saveValue}>
+            Save
+          </Button>
+        </div>
+      </Paper>
+    </>
   );
 }
 
--- SunrightHub/src/components/Inventory/Inventory.tsx
+++ SunrightHub/src/components/Inventory/Inventory.tsx
@@ -4,35 +4,27 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { Container } from "@mui/material";
 
 //To create a table entry/cell, make it easier, something to also put on top
-function createDataEntry(
-  BeginningOfWeek: number, // Beginning of Week, auto-update logic needs to be implemented separately
-  InventoryAdded: number,
-  EndOfWeek: number,
-  OrderByBox: number // Order in units, needs additional logic if required
-) {
-  let Usage = BeginningOfWeek + InventoryAdded - EndOfWeek;
-  return { BeginningOfWeek, InventoryAdded, EndOfWeek, Usage, OrderByBox };
-}
 
 const columns: GridColDef<(typeof rows)[number]>[] = [
   { field: "id", headerName: "ID", width: 90 },
+  { field: "name", headerName: "name", width: 400, editable: true },
   {
     field: "weekStart",
     headerName: "Week Start",
-    width: 150,
+    width: 100,
     editable: true,
   },
   {
     field: "InvAdd",
     headerName: "Inventory Added",
-    width: 150,
+    width: 100,
     editable: true,
   },
   {
     field: "weekEnd",
     headerName: "Week End",
     type: "number",
-    width: 110,
+    width: 100,
     editable: true,
   },
   {
@@ -40,43 +32,243 @@ const columns: GridColDef<(typeof rows)[number]>[] = [
     headerName: "Usage",
     description: "This column has a value getter and is not sortable.",
     sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.weekStart + row.InvAdd - row.weekEnd}`,
+    width: 100,
+    valueGetter: (value, row) =>
+      `${Number(row.weekStart) + Number(row.InvAdd) - Number(row.weekEnd)}`,
+  },
+  {
+    field: "orderByBox",
+    headerName: "Order By Box",
+    type: "number",
+    width: 100,
+    editable: true,
+  },
+  {
+    field: "amountByDelivery",
+    headerName: "Amount By Delivery",
+    type: "number",
+    width: 100,
+    editable: true,
   },
 ];
 
 const rows = [
-  { id: 1, weekStart: 41, InvAdd: 3, weekEnd: 2 },
-  { id: 2, weekStart: 12, InvAdd: 3, weekEnd: 31 },
-  { id: 3, weekStart: 31, InvAdd: 3, weekEnd: 32 },
-  { id: 4, weekStart: 41, InvAdd: 3, weekEnd: 42 },
-  { id: 5, weekStart: 61, InvAdd: 3, weekEnd: 91 },
-  { id: 6, weekStart: 23, InvAdd: 3, weekEnd: 24 },
-  { id: 7, weekStart: 3, InvAdd: 3, weekEnd: 31 },
-  { id: 8, weekStart: 5, InvAdd: 3, weekEnd: 42 },
-  { id: 9, weekStart: 61, InvAdd: 3, weekEnd: 4 },
+  {
+    id: 1,
+    name: "Boba-Brown (6/box)",
+    weekStart: 40.0,
+    InvAdd: 42.0,
+    weekEnd: 40.0,
+  },
+  {
+    id: 2,
+    name: "Boba-White (6/box)",
+    weekStart: 18.0,
+    InvAdd: 18.0,
+    weekEnd: 9.0,
+  },
+  {
+    id: 3,
+    name: "Cream Pudding Powder (20/box)",
+    weekStart: 20.0,
+    InvAdd: 0,
+    weekEnd: 17.0,
+  },
+  { id: 4, name: "Mochi Powder (20/box)", weekStart: 0, InvAdd: 0, weekEnd: 0 },
+  {
+    id: 5,
+    name: "Cheese Foam Powder (20/box)",
+    weekStart: 12.0,
+    InvAdd: 0,
+    weekEnd: 9.0,
+  },
+  {
+    id: 6,
+    name: "Pudding Powder (20/box)",
+    weekStart: 28.0,
+    InvAdd: 0,
+    weekEnd: 21.0,
+  },
+  {
+    id: 7,
+    name: "Casava/Starch Powder (20/box)",
+    weekStart: 40.0,
+    InvAdd: 0,
+    weekEnd: 40.0,
+  },
+  {
+    id: 8,
+    name: "Plain Jelly Powder (10/box)",
+    weekStart: 2.0,
+    InvAdd: 20.0,
+    weekEnd: 40.0,
+  },
+  {
+    id: 9,
+    name: "Non Dairy Creamer (20/box)",
+    weekStart: 75.0,
+    InvAdd: 40.0,
+    weekEnd: 56.0,
+  },
+  {
+    id: 10,
+    name: "Instant Q Jelly Powder (10/box)",
+    weekStart: 0,
+    InvAdd: 0,
+    weekEnd: 10.0,
+  },
+  {
+    id: 11,
+    name: "Black Sesame (8/box)",
+    weekStart: 0,
+    InvAdd: 0,
+    weekEnd: 24.0,
+  },
+  {
+    id: 12,
+    name: "Grapefruit Syrup (4/box)",
+    weekStart: 2.0,
+    InvAdd: 4.0,
+    weekEnd: 1.0,
+  },
+  {
+    id: 13,
+    name: "Passionfruit Syrup (4/box)",
+    weekStart: 7.0,
+    InvAdd: 4.0,
+    weekEnd: 7.0,
+  },
+  {
+    id: 14,
+    name: "Mango Syrup (4/box)",
+    weekStart: 8.0,
+    InvAdd: 8.0,
+    weekEnd: 8.0,
+  },
+  {
+    id: 15,
+    name: "Black Sugar (box)",
+    weekStart: 5.0,
+    InvAdd: 5.0,
+    weekEnd: 5.0,
+  },
+  {
+    id: 16,
+    name: "Wintermelon Bricks (20/box)",
+    weekStart: 41.0,
+    InvAdd: 0,
+    weekEnd: 30.0,
+  },
+  {
+    id: 17,
+    name: "White Peach Oolong Tea (30/box)",
+    weekStart: 34.0,
+    InvAdd: 0,
+    weekEnd: 28.0,
+  },
+  {
+    id: 18,
+    name: "Ceylon Black Tea (96/box)",
+    weekStart: 1.083,
+    InvAdd: 1.0,
+    weekEnd: 1.5,
+  },
+  {
+    id: 19,
+    name: "Jasmine Tea (108/box)",
+    weekStart: 0.7,
+    InvAdd: 1.0,
+    weekEnd: 0.5,
+  },
+  {
+    id: 20,
+    name: "Roasted Oolong Tea (108/box)",
+    weekStart: 1.0,
+    InvAdd: 0,
+    weekEnd: 0.75,
+  },
+  {
+    id: 21,
+    name: "Four Seasons Tea (142/box)",
+    weekStart: 2.2,
+    InvAdd: 1.0,
+    weekEnd: 2.5,
+  },
+  {
+    id: 22,
+    name: "Thai Tea (30/box)",
+    weekStart: 43.0,
+    InvAdd: 0,
+    weekEnd: 36.0,
+  },
+  {
+    id: 23,
+    name: "Honey Syrup (6/box)",
+    weekStart: 15.0,
+    InvAdd: 6.0,
+    weekEnd: 14.0,
+  },
+  {
+    id: 24,
+    name: "Grass Jelly Can (6/box) Herb juice",
+    weekStart: 5.0,
+    InvAdd: 0,
+    weekEnd: 3.0,
+  },
+  { id: 25, name: "Red Bean (18/box)", weekStart: 0, InvAdd: 0, weekEnd: 0 },
+  {
+    id: 26,
+    name: "Condensed Milk (Large) 3/box",
+    weekStart: 36.0,
+    InvAdd: 15.0,
+    weekEnd: 37.0,
+  },
+  {
+    id: 27,
+    name: "Condensed Milk (Small) 24/box",
+    weekStart: 26.0,
+    InvAdd: 24.0,
+    weekEnd: 24.0,
+  },
+  {
+    id: 28,
+    name: "Liquid Cane Sugar (4/box)",
+    weekStart: 22.0,
+    InvAdd: 44.0,
+    weekEnd: 46.0,
+  },
+  {
+    id: 29,
+    name: "Agar Boba (6/box)",
+    weekStart: 30.0,
+    InvAdd: 0,
+    weekEnd: 19.0,
+  },
+  {
+    id: 30,
+    name: "Lychee Jelly (4/box)",
+    weekStart: 26.0,
+    InvAdd: 0,
+    weekEnd: 13.0,
+  },
 ];
 
 function Inventory() {
   return (
-    <Container>
-      <Box sx={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 5,
-              },
-            },
-          }}
-          pageSizeOptions={[5]}
-          checkboxSelection
-          disableRowSelectionOnClick
-        />
-      </Box>
-    </Container>
+    <Box sx={{ height: 1000, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        checkboxSelection
+        disableRowSelectionOnClick
+        sx={{
+          "& .MuiDataGrid-columnHeader": {
+            fontSize: "10px", // Adjust font size
+            fontWeight: "bold", // Optional: Make headers bold
+          },
+        }}
+      />
+    </Box>
   );
 }
 
