d82ba35452194d8fed589af635d624c97f599574
lestersanjuan <lsanjua1@uci.edu>
2025-05-27 09:42:49 -0700

    Changed checkbox color

--- SunrightHub/src/components/DailyReport1/Report.tsx
+++ SunrightHub/src/components/DailyReport1/Report.tsx
@@ -1,16 +1,38 @@
 import React from "react";
+import { FormControlLabel, Checkbox } from "@mui/material";
 import "./Report.css";
+import { yellow } from "@mui/material/colors";
+
 function Report() {
   return (
     <div>
       <span>
-        <h3 className="inline">Store Operations:</h3>
-        <input type="checkbox" name="storeOperations" />
-        <label>Tea Quality</label>
-        <input type="checkbox" name="storeOperations" />
-        <label>Boba Quality</label>
-        <input type="checkbox" name="storeOperations" />
-        <label>Weekly Announcements spoke with Team</label>
+        <h3 className="top">Store Operations:</h3>
+        <div className="top items">
+          <FormControlLabel
+            control={<Checkbox defaultChecked className="customCheckbox" />}
+            label="Tea Quality"
+          />
+          <FormControlLabel
+            control={<Checkbox defaultChecked className="customCheckbox" />}
+            label="Boba Quality"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                defaultChecked
+                sx={{
+                  color: yellow[800],
+                  "&.Mui-checked": {
+                    color: yellow[600],
+                  },
+                }}
+                className="customCheckBox"
+              />
+            }
+            label="Weekly Announcements spoke with Team"
+          />
+        </div>
       </span>
     </div>
   );
